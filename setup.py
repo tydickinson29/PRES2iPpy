c9e44a1256137ae3db93a5af8e8839a4cdead2f6
Ty Dickinson <ty.dickinson@ou.edu>
2019-07-19 15:00:26 -0500

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PRES2iPpy',
+      version='0.5',
+      author='Ty Dickinson',
+      author_email='ty.dickinson@ou.edu',
+      description='Prediction of Rainfall Extremes at Subseasonal to Seasonal Periods in Python',
+      url='https://github.com/tydickinson29/PRES2iPpy',
+      packages['analysis','models']
+      )
