848f9cedbf261569f9dbdfefd91742d7b3a75542
Ty Dickinson <ty.dickinson@ou.edu>
2019-08-14 10:32:34 -0500

    Update file to make shapefile

--- pres2ippy/analysis/generateDatabase.py
+++ pres2ippy/analysis/generateDatabase.py
@@ -1,6 +1,22 @@
 import rainpy as rp
 import pandas as pd
-import time
+import numpy as np
+#import time
+#from tqdm import tqdm
+import geopandas
+
+months=[12,12,12,12,12,12,12,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+days=[25,26,27,28,29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+years=[1998,1998,1998,1998,1998,1998,1998,1999,1999,1999,1999,1999,1999,1999,1999,1999,1999,1999,1999,1999,1999]
+
+arr = np.zeros((len(months),261,622))*np.nan
+#for i in tqdm(range(1,32,1)):
+for i in range(len(months)):
+    print(days[i])
+    date = rp.DateTest(month=months[i], day=days[i], year=years[i])
+    date.kde()
+    arr[i,:,:] = date.Z
+level = np.nanpercentile(arr, q=95)
 
 begin = []
 end = []
@@ -10,16 +26,17 @@ miny = []
 maxx = []
 maxy = []
 area = []
-for i in range(1,32,1):
-    #t1 = time.time()
-    date = rp.DateTest(month=1, day=i, year=1999)
-    date.getObs()
-    date.checkRainyDays()
-    date.check3DayTotals()
+areaAvgRain = []
+maxTotalRain = []
+maxDailyRain = []
+#for i in tqdm(range(1,32,1)):
+for i in range(len(months)):
+    print(days[i])
+    date = rp.DateTest(month=months[i], day=days[i], year=years[i])
     date.kde()
-    date.getContours()
+    date.getContours(ticks=level)
     date.calcAreas()
-    #print('Time elapsed for day %s:%s'%(i, time.time()-t1))
+    date.maskOutsideRegion()
 
     if len(date.polys) > 0:
         begin.append(['%s/%s/%s'%(date.month, date.day, date.year)]*len(date.polys))
@@ -29,7 +46,10 @@ for i in range(1,32,1):
         miny.append([i.bounds[1] for i in date.polys])
         maxx.append([i.bounds[2] for i in date.polys])
         maxy.append([i.bounds[3] for i in date.polys])
-        area.append([num for num in date.areas[date.areas.keys()[0]] if num >= 100000])
+        area.append([num for num in date.areas[list(date.areas.keys())[0]] if num >= 100000])
+        areaAvgRain.append(date.weightedTotal)
+        maxTotalRain.append(np.nanmax(date.regionsTotal,axis=-1))
+        maxDailyRain.append(np.nanmax(date.regionsDaily,axis=-1))
 
 begin = [item for sublist in begin for item in sublist]
 end = [item for sublist in end for item in sublist]
@@ -39,10 +59,23 @@ miny = [item for sublist in miny for item in sublist]
 maxx = [item for sublist in maxx for item in sublist]
 maxy = [item for sublist in maxy for item in sublist]
 area = [item for sublist in area for item in sublist]
+areaAvgRain = [item for sublist in areaAvgRain for item in sublist]
+maxTotalRain = [item for sublist in maxTotalRain for item in sublist]
+maxDailyRain = [item for sublist in maxDailyRain for item in sublist]
 
-cols = ['Begin_Date','End_Date','Area','Min_Lon','Min_Lat','Max_Lon','Max_Lat']
-df = pd.DataFrame({'Begin_Date':begin, 'End_Date':end, 'Area':area,
-                   'Min_Lon':minx, 'Min_Lat':miny, 'Max_Lon':maxx, 'Max_Lat':maxy},
+def rounder(data):
+    return [np.around(i,decimals=2) for i in data]
+
+cols = ['Begin_Date', 'End_Date', 'Area', 'Area_Averaged_Precip', 'Maximum_Total_Precip',
+        'Maximum_1_Day_Precip', 'Min_Lon', 'Min_Lat', 'Max_Lon', 'Max_Lat']
+df = pd.DataFrame({'Begin_Date':begin, 'End_Date':end, 'Area':rounder(area),
+                   'Area_Averaged_Precip':rounder(areaAvgRain), 'Maximum_Total_Precip':rounder(maxTotalRain),
+                   'Maximum_1_Day_Precip':rounder(maxDailyRain), 'Min_Lon':rounder(minx), 'Min_Lat':rounder(miny),
+                   'Max_Lon':rounder(maxx), 'Max_Lat':rounder(maxy)},
                    columns=cols)
 df = df.astype({'Begin_Date':'datetime64[ns]', 'End_Date':'datetime64[ns]'})
 df.to_csv('database.csv')
+df.to_html('database.html')
+
+gdf = geopandas.GeoDataFrame(df, geometry=polys)
+gdf.to_file('database.shp')
