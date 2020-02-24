bd88992ea52877210918f7a1063761b2c8c4e9c5
Ty Dickinson <ty.dickinson@ou.edu>
2020-02-24 09:39:45 -0600

    Change scheme to a correlation approach and add recursion until convergence

--- pres2ippy/analysis/postprocess.py
+++ pres2ippy/analysis/postprocess.py
@@ -1,84 +1,83 @@
 import numpy as np
+import salem
+import xarray as xr
 import pandas as pd
 import shapely.wkt
-import geopandas
+from scipy import stats
 import glob
+import geopandas
 
-files = glob.glob('/share/data1/ty/database/*.csv')
+def groupEvents(arr, thresh):
+    coefs = np.zeros((arr.shape[0]))
+    for i in range(coefs.shape[0]):
+        coefs[i], _ = stats.pearsonr(x=arr[0,:], y=arr[i,:])
+    iloc = np.where(coefs >= thresh)[0]
+    return coefs, iloc
+
+def createMaskedArray(df):
+    targetRow = df.iloc[0]
+    subset = df.iloc[np.where((targetRow.Begin_Date <= df.Begin_Date) & (targetRow.End_Date >= df.Begin_Date))]
+    polys = [shapely.wkt.loads(i) for i in subset.geometry]
+
+    lat = np.arange(24, 50.1, 0.1)
+    lon = np.arange(232, 294.1, 0.1)
+    numRegions = len(subset)
+
+    gridLons, gridLats = np.meshgrid(lon, lat)
+    regions = np.ones_like(gridLats)
+    regions = np.tile(regions, (numRegions,1)).reshape(numRegions,lat.size,lon.size)
+
+    regions = xr.DataArray(regions, dims=('regions','lat','lon'), coords={'regions':range(numRegions),'lat':lat,'lon':lon})
+
+    regionsMasked = np.ones_like(regions)
+    for i in range(numRegions):
+        regionsMasked[i,:,:] = regions[i,:,:].salem.roi(geometry=polys[i])
+
+    regionsMasked = np.ma.masked_array(regionsMasked, np.isnan(regionsMasked))
+    regionsMasked = regionsMasked.filled(0)
+
+    n,y,x = regionsMasked.shape
+    regionsMasked = regionsMasked.reshape(n,y*x)
+    return regionsMasked
+
+files = glob.glob('/share/data1/Students/ty/database/*.csv')
 df = pd.concat([pd.read_csv(i) for i in files])
-df = df[df.Area >= 200000]
 df = df.astype({'Begin_Date':'datetime64[ns]', 'End_Date':'datetime64[ns]'})
 df = df.sort_values('Begin_Date')
+df.drop_duplicates(inplace=True)
 df.reset_index(drop=True, inplace=True)
-events = []
 
-def findEvent(df):
-    """Group events based on first row in preprocessed dataframe. All events that start on or
-    before the ending date of the event in the first row are initially considered. Then, events
-    are grouped if the interesting area is 50% of either intersectand area. Returns the chosen
-    event and the indices of grouped events.
-    """
-    polys = [shapely.wkt.loads(i) for i in df.geometry]
+while True:
+    numEvents = len(df)
+    events = []
+    while len(df) != 0:
+        #first create matrix for correlation
+        maskedEvents = createMaskedArray(df)
 
-    #create symmetrical matrix of all intersections in first subset
-    intersection = [[i.intersection(j) for j in polys] for i in polys]
+        #calculate correlations and pull out groupedEvents
+        coefs, iloc = groupEvents(arr=maskedEvents, thresh=0.5)
+        similarEvents = df.iloc[iloc]
+        similarEvents.reset_index(drop=True, inplace=True)
 
-    #find fractions relative to both events making intersection; no intersection gives 0
-    #rows are relative to one event, columns relative to the other
-    fracAreas = np.array([[j.area / polys[i].area for j in intersection[i]] for i in range(len(intersection))])
+        #choose event with largest TOE
+        event = similarEvents.iloc[similarEvents['Total_Over_Extreme'].idxmax]
+        events.append(event)
 
-    #find all polygons that group with the first event then find all polygons that group with all those events
-    tmpSim = np.where((fracAreas[0,:] >= 0.5) | (fracAreas[:,0] >= 0.5))[0]
-    indices = []
-    for i in tmpSim:
-        indices.append(np.where((fracAreas[i,:] >= 0.5) | (fracAreas[:,i] >= 0.5))[0])
+        #remove events with correlation >= 0.5
+        df.drop(iloc, inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        print(len(df))
 
-    indices = np.unique(np.concatenate(indices))
-    similarEvents = df.iloc[indices]
-    similarEvents.reset_index(drop=True, inplace=True)
-    event = similarEvents.iloc[similarEvents['Total_Over_Extreme'].idxmax]
-    return event, indices
-
-while len(df) != 0:
-    tmpEvents = []
-    tmpIndices = []
-    tmpDFs = []
-    #find first event
-    localDF = df.iloc[np.where((df.iloc[0].Begin_Date <= df.Begin_Date) & (df.iloc[0].End_Date >= df.Begin_Date))]
-    output = findEvent(localDF)
-    tmpEvents.append(output[0])
-    tmpIndices.append(output[1])
-    tmpDFs.append(localDF.iloc[tmpIndices[0]])
-
-    #find next event(s); repeat until convergence
-    localCounter = 0
-    while True:
-        localDF = df.iloc[np.where((tmpEvents[localCounter].Begin_Date <= df.Begin_Date) & (tmpEvents[localCounter].End_Date >= df.Begin_Date))]
-        #sort to ensure that previously identified event is in first row
-        localDF = localDF.sort_values(by=['Begin_Date','Total_Over_Extreme'], ascending=[True,False])
-        output = findEvent(localDF)
-        tmpEvents.append(output[0])
-        tmpIndices.append(output[1])
-        tmpDFs.append(localDF.iloc[tmpIndices[localCounter+1]])
-        if tmpEvents[localCounter].equals(tmpEvents[localCounter+1]):
-            break
-        localCounter += 1
-
-    allEvents = pd.concat(tmpDFs)
-    allEvents.drop_duplicates(inplace=True)
-    events.append(tmpEvents[localCounter])
-    df.drop(allEvents.index.tolist(), inplace=True)
+    print('\n')
+    df = pd.DataFrame(events)
+    df = df.sort_values('Begin_Date')
     df.reset_index(drop=True, inplace=True)
-    print(len(df))
+    if len(df) == numEvents:
+        break
 
+polys = [shapely.wkt.loads(i) for i in df.geometry]
+df.to_csv('/share/data1/Students/ty/database/database_14_v1.2.csv', index=False)
 
-events = pd.DataFrame(events)
-events = events.sort_values(by='Begin_Date')
-events.drop_duplicates(inplace=True)
-events.reset_index(drop=True, inplace=True)
-polys = [shapely.wkt.loads(i) for i in events.geometry]
-events.to_csv('/share/data1/ty/database/database_v1.0.csv', index=False)
-
-events = events.astype({'Begin_Date':str, 'End_Date':str})
-gdf = geopandas.GeoDataFrame(events, geometry=polys)
-gdf.to_file('/share/data1/ty/database/database_v1.0.shp')
+df = df.astype({'Begin_Date':str, 'End_Date':str})
+gdf = geopandas.GeoDataFrame(df, geometry=polys)
+gdf.to_file('/share/data1/Students/ty/database/database_14_v1.2.shp')
